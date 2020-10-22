3b69870ca206575e20bb8f86a08444dc474fdc4b
Jean-Philippe Dery <jeanphilippe.dery@gmail.com>
2020-10-21 21:02:24 -0400

    Fixed TS build errors

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,6 +1,6 @@
-export { generateAudioThumbnail } from 'audio/generateAudioThumbnail';
-export { generateVideoThumbnail } from 'video/generateVideoThumbnail';
-export { generateImageThumbnail } from 'image/generateImageThumbnail';
-export { getAudioMetadata } from 'audio/getAudioMetadata';
-export { getVideoMetadata } from 'video/getVideoMetadata';
-export { getImageMetadata } from 'image/getImageMetadata';
+export { generateAudioThumbnail } from './audio/generateAudioThumbnail';
+export { generateVideoThumbnail } from './video/generateVideoThumbnail';
+export { generateImageThumbnail } from './image/generateImageThumbnail';
+export { getAudioMetadata } from './audio/getAudioMetadata';
+export { getVideoMetadata } from './video/getVideoMetadata';
+export { getImageMetadata } from './image/getImageMetadata';
